bff3d5220468f29b36060e809bad8a6128151b5a
unknown <farhanaaktersuci@gmail.com>
2025-02-06 17:27:14 +0600

    File Added

---
+++ Simulation1.cs
@@ -0,0 +1,66 @@
+﻿using AventStack.ExtentReports;
+using AventStack.ExtentReports.Reporter;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+
+IWebDriver driver = new ChromeDriver();
+ExtentReports extent = new ExtentReports();
+
+//ExtentSparkReporter htmlreporter = new ExtentSparkReporter(@"D:\ReportResult\Report.html");
+ExtentSparkReporter htmlreporter = new ExtentSparkReporter(@"D:\ReportResult\Report" + DateTime.Now.ToString("ddMMyyy_hhmmttt") + "Report.html");
+
+
+extent.AttachReporter(htmlreporter);
+
+ExtentTest test = extent.CreateTest("Test case 1", "Positive LogIn test");
+
+driver.Navigate().GoToUrl("https://dev.ontrak.onsite.com.au/dashboard");
+
+//test.Log(Status.Info);
+
+test.Log(Status.Info, "Open url");
+
+Console.WriteLine("Open url");
+
+driver.Manage().Window.Maximize();
+
+Thread.Sleep(10000);
+driver.FindElement(By.CssSelector("#Username")).SendKeys("ju");
+Console.WriteLine("Type username  into Username field");
+test.Log(Status.Info, "Farhana Akter Suci");
+
+driver.FindElement(By.CssSelector("#btn-login")).Click();
+
+test.Log(Status.Info, "Push Submit button");
+Console.WriteLine("Push Submit button");
+
+Thread.Sleep(10000);
+
+driver.FindElement(By.CssSelector("#Password")).SendKeys("ju");
+
+test.Log(Status.Info, "Type password ju into Password field");
+
+Console.WriteLine("Type password ju into Password field");
+
+driver.FindElement(By.CssSelector("#btn-login")).Click();
+
+test.Log(Status.Info, "Push Submit button");
+Console.WriteLine("Push Submit button");
+
+try
+{
+    Thread.Sleep(10000);
+    driver.FindElement(By.CssSelector("#rent-activity"));
+    test.Log(Status.Pass, "Login successfully");
+}
+
+catch
+{
+
+    test.Log(Status.Fail, "Login failed");
+}
+
+extent.Flush();
+
+driver.Quit();
